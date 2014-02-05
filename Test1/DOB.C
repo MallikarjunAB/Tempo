d41218aa9efbeee7d13fbecdccb6ba93d378e651
Mallikarjun <ab.mallikarjun@gmail.com>
2014-02-05 17:31:10 +0530

    blanc is edited

---
+++ Test1/DOB.C
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <conio.h>
+
+void main()
+
+{
+
+int i,j,k=0;
+printf("Enter your date only (DD) :");
+scanf ("%d", &i);
+printf("Enter your Month only (MM) :");
+scanf ("%d", &j);
+printf("Enter your Year only (YYYY) :");
+scanf ("%d", &k);
+printf(" your DOB is %d: %d : %d ",i,j,k);
+getch();
+}
